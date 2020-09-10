55d68f155004d1188766d27fca7f1ca680fbdada
rebeckamolin <56819811+rebeckamolin@users.noreply.github.com>
2020-09-10 14:09:13 +0200

    add switch for eventlistpage and loginPage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
-
+import EventListPage from "./pages/EventListPage";
 
 function App() {
-
   return (
     <div>
-<LoginPage />
+      <Switch>
+        <Route path="/event-list">
+          <EventListPage />
+        </Route>
+
+        <Route path="/">
+          <LoginPage />
+        </Route>
+      </Switch>
+      <LoginPage />
     </div>
   );
 }
