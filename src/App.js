39f20369535d9a37a4e55d2d49dc17d676053fa7
rebeckamolin <56819811+rebeckamolin@users.noreply.github.com>
2020-09-10 14:03:47 +0200

    event och login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
-import LoginForm from "./components/LoginForm";
+import LoginPage from "./pages/LoginPage";
 
 
 function App() {
 
   return (
     <div>
-      <h1>Event App</h1>
-      <LoginForm />
-      
+<LoginPage />
     </div>
   );
 }
---
+++ src/pages/EventListPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function EventList() {
+  return (
+    <div>
+      <h1>Event List</h1>
+    </div>
+  );
+}
