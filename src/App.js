ec7af94cb253f01a7919e7053ee59762ad11869f
rebeckamolin <56819811+rebeckamolin@users.noreply.github.com>
2020-09-10 11:56:14 +0200

    refactor input handling state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 const ROOT_URL = "http://yoshi.willandskill.eu:8999/api/v1/";
 const LOGIN_URL = `${ROOT_URL} + auth/api-token-auth/`;
 
 function App() {
-  const emailInput = useRef(null);
-  const passwordInput = useRef(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   function login() {
-    const email = emailInput.current.value;
-    const password = passwordInput.current.value;
-    console.log(email, password);
-
+    console.log(email, password)
     fetch(LOGIN_URL);
   }
   return (
@@ -20,15 +17,11 @@ function App() {
       <div>
         <div>
           <label htmlFor="email">Email</label>
-          <input
-            ref={emailInput}
-            name="email"
-            placeholder="rebecka@molin.com"
-          />
+          <input name="email" type="email" onChange={(event) => setEmail(event.currentTarget.value)} placeholder="rebecka@molin.com" />
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input ref={passwordInput} name="password" type="password" />
+          <input name="password" type="password" onChange={(event) => setPassword(event.currentTarget.value)}/>
         </div>
         <button onClick={login}>Login</button>
       </div>
