c6cd32993582ad2f20c03e36f2aabbb122d0ca28
rebeckamolin <56819811+rebeckamolin@users.noreply.github.com>
2020-09-10 11:36:10 +0200

    other

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Event App
-
 ## API 
-
 ROOT URL: http://yoshi.willandskill.eu:8999/api/v1/
-
 LOGIN URL: ROOT URL + auth/api-token-auth/
-
 EVENT LIST URL: ROOT URL + events/events/
-
 EVENT PAGE LIST: ROOT URL + cms/{event_slug}/pages
-
 EVENT MAIN PAGE: ROOT URL + cms/{event_slug}/main-page/
+**Credentials:**
+email: test.user@willandskill.se
+password: js-lesson-10
+## Externa paket att installera
+* react-router-dom
+* styled-components
--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useRef } from "react";
+
+const ROOT_URL = "http://yoshi.willandskill.eu:8999/api/v1/";
+const LOGIN_URL = `${ROOT_URL} + auth/api-token-auth/`;
 
 function App() {
   const emailInput = useRef(null);
   const passwordInput = useRef(null);
 
-  function login(){
-    console.log("login")
+  function login() {
+    fetch(LOGIN_URL);
   }
   return (
     <div>
@@ -21,7 +24,7 @@ function App() {
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input name="password" type="password" />
+          <input ref={passwordInput} name="password" type="password" />
         </div>
         <button onClick={login}>Login</button>
       </div>
